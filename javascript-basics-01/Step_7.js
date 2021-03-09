f99ab4ffa4585853291c9d44d0c6ba3bf6e58228
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-03-10 00:47:42 +0200

    Step_7 Done

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,7 @@
+function Result() {
+    var a = document.getElementById('shoe_size').value;
+    var b = document.getElementById('year').value;
+    
+    
+var d= ((2*a)+5)*50 -b + 1766;
+alert("The result is: " +d)}
