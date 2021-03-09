8535207184f483c34bb4c0a89792c072f94079d8
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-03-10 00:42:18 +0200

    Step_6 Done

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,7 @@
+function Result() {
+    var a = document.getElementById('first_number').value;
+    var b = document.getElementById('second_number').value;
+    
+    
+var d= a%b;
+alert("The result is: " +d)}
