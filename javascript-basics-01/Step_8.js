036569bfb62b7af2070c4cbeb58c9f2e266e2847
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-03-10 00:52:16 +0200

    Step_8 Done

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,10 @@
+function Result() {
+    var a = document.getElementById('age').value;
+       
+if (a>18) {
+    alert("you are over 18")
+    
+        } else {
+            alert("you are under 18")
+                }
+}
