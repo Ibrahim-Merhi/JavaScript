7adf5c5d77cf1df0f29b389b13d50002a5799eab
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-03-10 00:31:22 +0200

    Step_4 Done

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,7 @@
+function validation() {
+var n = document.getElementById('name').value;
+var s = document.getElementById('surname').value;
+var c = document.getElementById('city').value;
+
+
+ alert("Hello : " + n + " " + s + "\n You are from : " + c );}
