2aba68708016f95e99f6de1a2771d88b531fbef6
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-03-11 19:07:40 +0200

    Step 3 Done

---
+++ javascript-basics-03/step 3/step 3.js
@@ -0,0 +1,10 @@
+
+function green(){
+	document.getElementById("text").style.color = 'green';
+}
+function red(){
+	document.getElementById("text").style.color = 'red';
+}
+function blue(){
+	document.getElementById("text").style.color = 'blue';
+}
